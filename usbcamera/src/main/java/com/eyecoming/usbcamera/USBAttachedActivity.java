b8eb25149c6ef83e4ae88f13a1f6715f6774d2a6
绝尘释爱 <juechenshiai@163.com>
2018-07-23 17:41:44 +0800

    添加USBAttachedActivity，USB设备自动检测并连接，首次需要确认权限，之后记住权限

---
+++ usbcamera/src/main/java/com/eyecoming/usbcamera/USBAttachedActivity.java
@@ -0,0 +1,31 @@
+package com.eyecoming.usbcamera;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * USBAttachedActivity
+ *
+ * @author JesseHu
+ * @date 2018/7/23
+ */
+public class USBAttachedActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Window window = getWindow();
+        WindowManager.LayoutParams wl = window.getAttributes();
+        //这句就是设置窗口里控件的透明度的．0.0全透明．1.0不透明．
+        wl.alpha = 0.0f;
+        window.setAttributes(wl);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        overridePendingTransition(0, 0);
+        super.onPause();
+    }
+}
