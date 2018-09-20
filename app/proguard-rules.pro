e1c56210a8cfba9f23594f634b4fe04a04f6b2db
绝尘释爱 <juechenshiai@163.com>
2018-09-20 17:41:29 +0800

    更新SDK 更新demo 更改UCamera回调 添加CameraHandler回调

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,71 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+-dontskipnonpubliclibraryclasses # 不忽略非公共的库类
+-optimizationpasses 5            # 指定代码的压缩级别
+-dontusemixedcaseclassnames      # 是否使用大小写混合
+-dontpreverify                   # 混淆时是否做预校验
+-verbose                         # 混淆时是否记录日志
+-keepattributes *Annotation*     # 保持注解
+-ignorewarning                   # 忽略警告
+-dontoptimize                    # 优化不优化输入的类文件
+
+-optimizations !code/simplification/arithmetic,!field/*,!class/merging/*  # 混淆时所采用的算法
+
+#保持哪些类不被混淆
+-keep public class * extends android.app.Activity
+-keep public class * extends android.app.Application
+-keep public class * extends android.app.Service
+-keep public class * extends android.content.BroadcastReceiver
+-keep public class * extends android.content.ContentProvider
+-keep public class * extends android.app.backup.BackupAgentHelper
+-keep public class * extends android.preference.Preference
+-keep public class com.android.vending.licensing.ILicensingService
+
+#生成日志数据，gradle build时在本项目根目录输出
+-dump class_files.txt            #apk包内所有class的内部结构
+-printseeds seeds.txt            #未混淆的类和成员
+-printusage unused.txt           #打印未被使用的代码
+-printmapping mapping.txt        #混淆前后的映射
+
+-keep public class * extends android.support.** #如果有引用v4或者v7包，需添加
+
+-keep class com.eyecoming.usblib.bean.**{*;}       #不混淆bean
+-keep class com.eyecoming.usblib.view.**{*;}       #不混淆自定义控件
+-dontwarn com.eyecoming.arwork.**              #忽略某个包的警告
+-keepattributes Signature        #不混淆泛型
+-keepnames class * implements java.io.Serializable #不混淆Serializable
+
+-keepclassmembers class **.R$* { #不混淆资源类
+public static <fields>;
+}
+-keepclasseswithmembernames class * {  # 保持 native 方法不被混淆
+native <methods>;
+}
+-keepclasseswithmembers class * {      # 保持自定义控件类不被混淆
+public <init>(android.content.Context, android.util.AttributeSet);
+}
+-keepclasseswithmembers class * {      # 保持自定义控件类不被混淆
+public <init>(android.content.Context, android.util.AttributeSet, int);
+}
+-keepclassmembers class * extends android.app.Activity { # 保持自定义控件类不被混淆
+public void *(android.view.View);
+}
+-keepclassmembers enum * {             # 保持枚举 enum 类不被混淆
+public static **[] values();
+public static ** valueOf(java.lang.String);
+}
+-keep class * implements android.os.Parcelable {         # 保持 Parcelable 不被混淆
+public static final android.os.Parcelable$Creator *;
+}
+
+
+#uvc
+-keepclassmembers public class com.serenegiant.usb.UVCCamera {
+	public *;
+	protected *;
+	private *;
+}
+-keep class com.serenegiant.usb.*{*;}
+-keep class com.eyecoming.usbcamera.widget.**{*;}
+-keep class com.eyecoming.usbcamera.usbcamerahandler.**{*;}
--- app/src/main/java/com/eyecoming/usblib/MainActivity.java
+++ app/src/main/java/com/eyecoming/usblib/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.res.Configuration;
 import android.graphics.SurfaceTexture;
 import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Surface;
@@ -12,20 +13,24 @@ import android.widget.Button;
 
 import com.eyecoming.usbcamera.OnCameraListener;
 import com.eyecoming.usbcamera.UCamera;
+import com.eyecoming.usbcamera.impl.CameraCallback;
 import com.eyecoming.usbcamera.usbcamerahandler.UVCCameraHandler;
 import com.eyecoming.usbcamera.widget.CameraViewInterface;
 import com.serenegiant.usb.CameraDialog;
 import com.serenegiant.usb.IFrameCallback;
+import com.serenegiant.usb.Size;
 import com.serenegiant.usb.USBMonitor;
 import com.serenegiant.usb.UVCCamera;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 /**
- * MainActivity
+ * Usb Camera SDK Demo
  *
  * @author JesseHu
  * @date 2018/7/19
+ * @update 2018/9/20
  */
 public final class MainActivity extends AppCompatActivity implements CameraDialog.CameraDialogParent {
 
@@ -42,79 +47,45 @@ public final class MainActivity extends AppCompatActivity implements CameraDialo
 
         setContentView(R.layout.activity_main);
 
+        initView();
+
+        setupUVCCamera();
+    }
+
+    private void initView() {
         Button mIntentBtn = findViewById(R.id.btn_intent);
         mUVCCameraView = findViewById(R.id.camera_view);
-        mUVCCameraView.setAspectRatio(PREVIEW_WIDTH / (float) PREVIEW_HEIGHT);
-        mCameraHandler = UVCCameraHandler.createHandler(this, mUVCCameraView, 1, PREVIEW_WIDTH, PREVIEW_HEIGHT, 1);
-        mUCamera = new UCamera(this, true, new OnCameraListener() {
-            @Override
-            public void connected(UVCCamera camera, USBMonitor.UsbControlBlock ctrlBlock) {
-                if (mCameraHandler != null) {
-                    mCameraHandler.open(ctrlBlock);
-                    startPreview();
-                }
-            }
-
-            @Override
-            public void disconnect() {
-
-            }
-
-            @Override
-            public void onAttach(UsbDevice device) {
-
-            }
-
-            @Override
-            public void onDettach(UsbDevice device) {
-
-            }
-        });
-
         mIntentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mCameraHandler.isPreviewing()) {
-//                    mCameraHandler.close();
                     mCameraHandler.stopPreview();
                     return;
                 }
-                //重新连接camera
-//                mUCamera.getFirstUsbCameraDevice();
+
                 startPreview();
             }
         });
-        mUVCCameraView.setCallback(new CameraViewInterface.Callback() {
-            @Override
-            public void onSurfaceCreated(CameraViewInterface view, Surface surface) {
-                if (mCameraHandler.isPreviewing()) {
-                    mCameraHandler.stopPreview();
-                }
-                //第一次打开activity
-                if (isFirst) {
-                    isFirst = false;
-                } else {
-//                    mUCamera.getFirstUsbCameraDevice();
-                    startPreview();
-                }
-            }
+    }
 
-            @Override
-            public void onSurfaceChanged(CameraViewInterface view, Surface surface, int width, int height) {
-
-            }
-
-            @Override
-            public void onSurfaceDestroy(CameraViewInterface view, Surface surface) {
-                Log.i("CameraViewInterface", "onSurfaceDestroy: ");
-            }
-        });
+    /**
+     * 设置Camera 预览尺寸以及Camera回调
+     */
+    private void setupUVCCamera() {
+        mUVCCameraView.setCallback(mCameraViewCallback);
+        mUVCCameraView.setAspectRatio(PREVIEW_WIDTH / (float) PREVIEW_HEIGHT);
+        // 预览尺寸需要Camera支持
+        mCameraHandler = UVCCameraHandler.createHandler(this, mUVCCameraView, 1, PREVIEW_WIDTH, PREVIEW_HEIGHT, 1);
+        mCameraHandler.addCallback(mCameraCallback);
+        mUCamera = new UCamera(this, true, mCameraListener);
     }
 
     private void startPreview() {
         SurfaceTexture st = mUVCCameraView.getSurfaceTexture();
         if (st != null) {
+            // 开启预览
             mCameraHandler.startPreview(new Surface(st));
+            // 设置Frame的回调以及Frame的数据格式
             mCameraHandler.setFrameCallback(mFrameCallback, UVCCamera.PIXEL_FORMAT_NV21);
         }
     }
@@ -150,8 +121,130 @@ public final class MainActivity extends AppCompatActivity implements CameraDialo
 
     }
 
+    private OnCameraListener mCameraListener = new OnCameraListener() {
+        @Override
+        public void connected(UsbDevice usbDevice, USBMonitor.UsbControlBlock ctrlBlock) {
+            if (mCameraHandler != null) {
+                // UVCCameraHandler.open()为异步
+                mCameraHandler.open(ctrlBlock);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 使用UVCCameraHandler.getUvcCamera()获取UVCCamera对象时需要确保UVCCameraHandler.open()已经执行完
+                        UVCCamera uvcCamera = mCameraHandler.getUvcCamera();
+                    }
+                }, 1000);
+                startPreview();
+            }
+        }
 
-    private final IFrameCallback mFrameCallback = new IFrameCallback() {
+        @Override
+        public void disconnect(UsbDevice usbDevice, USBMonitor.UsbControlBlock usbControlBlock) {
+
+        }
+
+        @Override
+        public void onAttach(UsbDevice device) {
+
+        }
+
+        @Override
+        public void onDettach(UsbDevice device) {
+            // 在USB设备断开的时候close UVCCameraHandler防止再次连接的时候打开Camera失败
+            if (mCameraHandler != null) {
+                if (mCameraHandler.isPreviewing()) {
+                    mCameraHandler.stopPreview();
+                }
+                if (mCameraHandler.isOpened()) {
+                    mCameraHandler.close();
+                }
+            }
+        }
+    };
+
+    /**
+     * CameraView回调
+     */
+    private CameraViewInterface.Callback mCameraViewCallback = new CameraViewInterface.Callback() {
+        @Override
+        public void onSurfaceCreated(CameraViewInterface view, Surface surface) {
+            if (mCameraHandler.isPreviewing()) {
+                mCameraHandler.stopPreview();
+            }
+            //第一次打开activity
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                startPreview();
+            }
+        }
+
+        @Override
+        public void onSurfaceChanged(CameraViewInterface view, Surface surface, int width, int height) {
+
+        }
+
+        @Override
+        public void onSurfaceDestroy(CameraViewInterface view, Surface surface) {
+            Log.i("CameraViewInterface", "onSurfaceDestroy: ");
+        }
+    };
+
+    /**
+     * CameraHandler回调
+     */
+    private CameraCallback mCameraCallback = new CameraCallback() {
+        @Override
+        public void onOpen(UVCCamera uvcCamera) {
+            //  UVCCameraHandler.open()执行完成后回调，同时返回当前UVCCamera对象
+
+            // 获取USB Camera支持的分辨率
+            // 方式一
+            String supportedSizeStr = uvcCamera.getSupportedSize();
+            // type参数可以通过解析supportedSizeStr，值为6或4
+            List<Size> supportedSize = UVCCamera.getSupportedSize(6, supportedSizeStr);
+
+            // 方式二
+            List<Size> supportedSizeList = uvcCamera.getSupportedSizeList();
+
+            // 方式三 通过解析json(supportedSizeStr)数据获取
+        }
+
+        @Override
+        public void onClose() {
+
+        }
+
+        @Override
+        public void onStartPreview() {
+
+        }
+
+        @Override
+        public void onStopPreview() {
+
+        }
+
+        @Override
+        public void onStartRecording() {
+
+        }
+
+        @Override
+        public void onStopRecording() {
+
+        }
+
+        @Override
+        public void onError(Exception e) {
+
+        }
+    };
+
+    /**
+     * 每帧图像的回调
+     */
+    private IFrameCallback mFrameCallback = new IFrameCallback() {
         @Override
         public void onFrame(final ByteBuffer frame) {
             //处理每一帧的数据
